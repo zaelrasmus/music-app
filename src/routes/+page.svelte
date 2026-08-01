fce7fbda7562e50ec7aba1650dc89209038466ec
zaelrasmus <zaelfox22@gmail.com>
2026-08-01 16:02:07 -0400

    build(deps): initialize shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-
-  let name = $state("");
-  let greetMsg = $state("");
-
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+    import { Button } from "$components/ui/button/index.js";
+    import { invoke } from "@tauri-apps/api/core";
 </script>
 
 <main class="container">
     <h1 class="text-red-600">Hello World</h1>
+    <Button> This is a button from shadcn-svelte</Button>
 </main>
